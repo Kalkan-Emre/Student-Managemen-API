669a16a6f2c2cb099138ae73aa00d59f5a1f8f38
Emre Kalkan <kalkan_emre@outlook.com>
2022-02-23 14:04:24 +0300

    First

--- src/main/java/com/example/student_managment/Student/StudentController.java
+++ src/main/java/com/example/student_managment/Student/StudentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class StudentController {
 
     @Autowired
-    private StudentService studentService; 
+    private StudentService studentService;
 
     @GetMapping
     public List<Students> getStudents(){
